1f5dda7f8b413e6ea2c92d787e18930c05c6b6c2
Humm <jhumm@mcw.edu>
2022-07-10 16:42:30 -0500

    feat:Update project urls + song urls + views in songs model

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . models import Song
+from . serializers import SongSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def music_list(request):
+    songs = Song.objects.all()
+    serializer = SongSerializer(songs, many=True)
+    return Response(serializer.data)
