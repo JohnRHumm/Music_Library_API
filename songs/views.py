8939233d06fac98a8dd357e445e334e174edf014
Humm <jhumm@mcw.edu>
2022-07-10 19:39:02 -0500

    feat:Add GET request by id to views.py song app

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,13 +8,21 @@ from . serializers import SongSerializer
 
 @api_view(['GET', 'POST'])
 def song_list(request):
-
     if request.method == 'GET':
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
-        serializer = SongSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        serializer=SongSerializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+
+@ api_view(['GET'])
+def song_detail(request, pk):
+    
+    song=get_object_or_404(Song,pk = pk)
+    serializer=SongSerializer(song)
+    return Response(serializer.data, status = status.HTTP_200_OK)
+    
